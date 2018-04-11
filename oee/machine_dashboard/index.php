9fdcbc1f7065b2ae09376a055ea600183f9dd442
Anant <Anant@CPSG-3>
2018-04-11 12:27:50 +0530

    add Company, plant, workCenter, Machine and machineDashboard

--- oee/machine_dashboard/index.php
+++ oee/machine_dashboard/index.php
@@ -40,9 +40,10 @@ tempData.oeeDash=
 
 $(document).ready(function() {
 debugger; 
+  $('.knob').knob();
 
 tempData.oeeDash.getImg();
-tempData.oeeDash.oeeCirclePerc('oeePerc',90,'#E29C21');
+tempData.oeeDash.oeeCirclePerc('oeePerc',75,'#E29C21');
 tempData.oeeDash.oeeCirclePerc('availPerc',40,'#FDCA6C');
 tempData.oeeDash.oeeCirclePerc('performPerc',20,'#DB4F31');
 tempData.oeeDash.oeeCirclePerc('qualityPerc',90,'#1AD34E');
@@ -114,8 +115,8 @@ $('#compProfile').click(function(e){
           </div>
           <div class="panel-body">  
             <div class="row">
-              <div class="col-md-12 col-xs-12 text-center">
-                <input type="text" class="knob" id="oeePerc" data-skin="tron" data-thickness="0.2" data-width="100" data-height="100" readonly>
+              <div class="col-md-12 col-xs-12 text-center" style="margin-bottom: 3%;">
+                <input type="text" class="knob" id="oeePerc" data-skin="tron" data-thickness="0.2" data-width="80" data-height="80" readonly>
               </div>
                
               <div class="col-md-8 col-xs-8">
@@ -138,10 +139,7 @@ $('#compProfile').click(function(e){
           <div class="panel-heading panelHeader">
             <div class="panel-title pull-left">
               <i class="fa fa-sliders fa-fw"></i> Availability
-            </div>
-            <div class="panel-title pull-right">
-              <div id="statusImg"></div>
-            </div>
+            </div>         
            <!--  <div class="panel-title pull-right">
                <i id="compProfile" class="btn btn-xs fa fa-expand" aria-hidden="true"></i>
               </div> -->
@@ -150,16 +148,48 @@ $('#compProfile').click(function(e){
           <div class="panel-body">  
             <div class="row">
               <div class="col-md-12 col-xs-12 text-center">
-                <input type="text" class="knob" id="availPerc" data-skin="tron" data-thickness="0.2" data-width="100" data-height="100" readonly>
+                <input type="text" class="knob" id="availPerc" data-skin="tron" data-thickness="0.2" data-width="80" data-height="80" readonly>
               </div>
                
-              <div class="col-md-8 col-xs-8">
-                <span id="showIobotImg"></span>  
-              </div>
+            <p class="text-center"> Planned Production Time </p>
+            <p class="text-center" style="font-size: 25px;margin: -5%;font-weight: bold;color: #013885;">07:00</p> 
+            
+            <div class="col-md-12 col-xs-12" style="margin-top:8%;    padding: 0px;">
+                <p class="col-md-7 col-xs-7 availTextRight">Run Time</p>
+                <p class="col-md-5 col-xs-5 text-right availTextLeft">04:17</p>
+                <div class="col-md-12 col-xs-12" style="margin-top: -10px; margin-bottom: 4%;">    
+                  <div class="progress progress-sm active" style="margin: auto;">
+                    <div class="progress-bar progress-bar-primary progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                      <span class="sr-only">20% Complete</span>
+                    </div>
+                  </div>
+                </div>
+            </div>
 
-              <div class="col-md-12 col-xs-12" style="margin-top: 6%; padding-right: 7px;">
-                    <span id="imgTitleInfo" style="font-size: 12px;"></span>
-              </div> 
+            <div class="col-md-12 col-xs-12" style="padding: 0px;">
+                <p class="col-md-7 col-xs-7 availTextRight">Idle Time</p>
+                <p class="col-md-5 col-xs-5 text-right availTextLeft">01:33</p>
+                <div class="col-md-12 col-xs-12" style="margin-top: -10px; margin-bottom: 4%;">    
+                  <div class="progress progress-sm active" style="margin: auto;">
+                    <div class="progress-bar progress-bar-primary progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 22%">
+                      <span class="sr-only">20% Complete</span>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 col-xs-12" style="padding: 0px;">
+                <p class="col-md-7 col-xs-7 availTextRight">Breakdown Time</p>
+                <p class="col-md-5 col-xs-5 text-right availTextLeft">01:15</p>
+                <div class="col-md-12 col-xs-12" style="margin-top: -10px; margin-bottom: 4%;">    
+                  <div class="progress progress-sm active" style="margin: auto;">
+                    <div class="progress-bar progress-bar-primary progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 55%">
+                      <span class="sr-only">20% Complete</span>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
             </div>      
           </div>
         </div>
@@ -184,16 +214,21 @@ $('#compProfile').click(function(e){
           <div class="panel-body">  
             <div class="row">
               <div class="col-md-12 col-xs-12 text-center">
-                <input type="text" class="knob" id="performPerc" data-skin="tron" data-thickness="0.2" data-width="100" data-height="100" readonly>
-              </div>
-               
-              <div class="col-md-8 col-xs-8">
-                <span id="showIobotImg"></span>  
+                <input type="text" class="knob" id="performPerc" data-skin="tron" data-thickness="0.2" data-width="80" data-height="80" readonly>
               </div>
 
-              <div class="col-md-12 col-xs-12" style="margin-top: 6%; padding-right: 7px;">
-                    <span id="imgTitleInfo" style="font-size: 12px;"></span>
-              </div> 
+            <div class="col-md-12 col-xs-12" style="padding: 0px;margin-top:5%;">
+                <p class="text-center timeFontStyle">04:12:01</p>
+                <p class="text-center">Ideal Cycle Time</p>
+            </div>
+
+
+            <div class="col-md-12 col-xs-12" style="padding: 0px;">
+                <p class="text-center timeFontStyle">04:12:01</p>
+                <p class="text-center">Average Time / Part</p>
+            </div>
+
+
             </div>      
           </div>
         </div>
@@ -215,16 +250,42 @@ $('#compProfile').click(function(e){
           <div class="panel-body">  
             <div class="row">
               <div class="col-md-12 col-xs-12 text-center">
-                <input type="text" class="knob" id="qualityPerc" data-skin="tron" data-thickness="0.2" data-width="100" data-height="100" readonly>
+                <input type="text" class="knob" id="qualityPerc" data-skin="tron" data-thickness="0.2" data-width="80" data-height="80" readonly>
               </div>
                
-              <div class="col-md-8 col-xs-8">
-                <span id="showIobotImg"></span>  
-              </div>
+          <!--   <div class="col-md-12 col-xs-12" style="padding: 0px;margin-top:5%;">
+                <p class="text-right timeFontStyle">04:12:01</p>
+                <p class="text-right">Ideal Cycle Time</p>
+            </div> -->
 
-              <div class="col-md-12 col-xs-12" style="margin-top: 6%; padding-right: 7px;">
-                    <span id="imgTitleInfo" style="font-size: 12px;"></span>
-              </div> 
+            <div class="col-md-12 col-xs-12" style="padding: 0px;margin-bottom: -7%;">
+                <p class="col-md-4 col-xs-4 availTextRight">
+                  <img src="../common/img/m_dashboard/sum_sigma.png" style="margin-top: 25%;"/> </p>
+              <div class="col-md-8 col-xs-8 ">
+                <p class="text-right timeFontStyle">10,854</p>
+                <p class="text-right">Total Count</p>
+              </div>
+            </div>
+
+            <div class="col-md-12 col-xs-12" style="padding: 0px;margin-bottom: -7%;">
+                <p class="col-md-4 col-xs-4 availTextRight">
+                  <img src="../common/img/m_dashboard/hand_up.png" style="margin-top: 25%;"/> </p>
+              <div class="col-md-8 col-xs-8 ">
+                <p class="text-right timeFontStyle">9,854</p>
+                <p class="text-right">OK Count</p>
+              </div>
+            </div>
+
+
+            <div class="col-md-12 col-xs-12" style="padding: 0px;">
+                <p class="col-md-4 col-xs-4 availTextRight">
+                  <img src="../common/img/m_dashboard/hand_down.png" style="margin-top: 25%;"/> </p>
+              <div class="col-md-8 col-xs-8 ">
+                <p class="text-right timeFontStyle">949</p>
+                <p class="text-right">Rejected Count</p>
+              </div>
+            </div>
+
             </div>      
           </div>
         </div>
