30b7a27b83aec7abf8610223f51b3128c872787f
User One <User One@DESKTOP-5N9CF97>
2018-04-26 17:22:44 +0530

    Reason Code No. field added.

--- oee/reasons/getDataController.php
+++ oee/reasons/getDataController.php
@@ -7,23 +7,21 @@
 /* ------------Reasons DB Operation --------------------- */
 
 if(isset($_POST['saveReasons'])){
-   
     $reason_id=$_POST['reason_id'];
     $reason_type_id=$_POST['reason_type_id'];
     $message=$_POST['message'];
     $color_code=$_POST['color'];
-    
+    $reason_code_no=$_POST['reason_code_no'];
     $table = 'sfs_reason_code';
-    $DataMarge=array('reason_type_id'=>$reason_type_id,
-                     'message'=>$message,
-                     'color_code'=>$color_code
-    );
-
     if($reason_id == ''){
+        $DataMarge=array('id'=>$reason_code_no,
+            'reason_type_id'=>$reason_type_id,
+            'message'=>$message,
+            'color_code'=>$color_code
+        );
         $sqlQuery = mysqli_insert_array($table, $DataMarge, "submit"); // Function say generate complete query
        // echo $sqlQuery;
-        $res=mysqli_query($con,$sqlQuery) or die('Error: ' . mysqli_error($con));
-        
+        $res=mysqli_query($con,$sqlQuery);
         if(!$res) {
             $error="Reason Code Already Exists";
             $response['info']=$error;
@@ -39,13 +37,16 @@ if(isset($_POST['saveReasons'])){
                 $response['info']=$error;
                 $response['infoRes']='E'; //Error
             }
-            
         }
-        
     }else{
+        $DataMarge=array('id'=>$reason_code_no,
+            'reason_type_id'=>$reason_type_id,
+            'message'=>$message,
+            'color_code'=>$color_code
+        );
         $cond=' id='.$reason_id;
         $sqlQuery = mysqli_update_array($table, $DataMarge, "submit",$cond); // Function say generate complete query
-        $res=mysqli_query($con,$sqlQuery); //or die('Error: ' . mysqli_error($con));
+        $res=mysqli_query($con,$sqlQuery);
         //echo $sqlQuery;
         if(!$res) {
             $error="Reason Code Already Exists";
@@ -127,18 +128,16 @@ if(isset($_POST['deleteReason'])){
 
 
 if(isset($_POST['saveReasonType'])){
-    
     $message=$_POST['message1'];
     $color_code=$_POST['color_code1'];
-    
-    $reason_id=$_POST['reason_id'];
-   
+    $reason_id1=$_POST['reason_id1'];
+    $r_id=$_POST['id_reason_type'];
     $table = 'sfs_reason_type';
-    $DataMarge=array('message'=>$message,
-                'color_code'=>$color_code
-    );
-
-    if($reason_id == ''){
+    if($r_id == ''){
+        $DataMarge=array('id'=>$reason_id1,
+            'message'=>$message,
+            'color_code'=>$color_code
+        );
         $sqlQuery = mysqli_insert_array($table, $DataMarge, "submit"); // Function say generate complete query
         $res=mysqli_query($con,$sqlQuery); //or die('Error: ' . mysqli_error($con));
         
@@ -161,9 +160,30 @@ if(isset($_POST['saveReasonType'])){
         }
         
     }else{
-        
+        $DataMarge=array(
+            'message'=>$message,
+            'color_code'=>$color_code
+        );
+        $cond=' id='.$reason_id1;
+        $sqlQuery = mysqli_update_array($table, $DataMarge, "submit",$cond); // Function say generate complete query
+        $res=mysqli_query($con,$sqlQuery); 
+        if(!$res) {
+            $error="Reason Code Type Already Exists";
+            $response['info']=$error;
+            $response['infoRes']='E'; //Error
+        }else {
+            if(mysqli_errno() != 1062){
+                move_uploaded_file($file_tmps,$filePath);
+                $response['info']="Record Updated Successfully";
+                $response['infoRes']="S"; // success
+                $response['mysqli_insert_id']=mysqli_insert_id($con);
+            }else{
+                $error="Reason Code Type Already Exists";
+                $response['info']=$error;
+                $response['infoRes']='E'; //Error
+            }
+        }
     }
-    
     $status['data'] = $response;
     echo json_encode($status);
     mysqli_close($con);
